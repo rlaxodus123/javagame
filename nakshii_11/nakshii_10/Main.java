16b51a6b6cc130919e2e0f6ceac47a8edb235dc4
rlaxodus123 <101035163+rlaxodus123@users.noreply.github.com>
2024-11-29 00:28:07 +0900

    Add files via upload

---
+++ nakshii_11/nakshii_10/Main.java
@@ -0,0 +1,14 @@
+package nakshii_10;
+
+public class Main {
+		public static final int SCREEN_WIDTH  = 1200;
+		
+		public static final int SCREEN_HEIGHT  = 720;
+	
+		public static void main(String[] args) {
+			
+			new Nakshicommon();
+			
+		}
+
+}
