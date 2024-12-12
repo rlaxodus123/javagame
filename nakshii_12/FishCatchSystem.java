62e94644419ea6310fe4ed2b703f76ec3d204b6f
rlaxodus123 <101035163+rlaxodus123@users.noreply.github.com>
2024-12-12 22:38:25 +0900

    Add files via upload

---
+++ nakshii_12/FishCatchSystem.java
@@ -0,0 +1,47 @@
+package nakshii_12;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class FishCatchSystem extends KeyAdapter{
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if(Nakshicommon.game == null) {
+				return;
+			}
+			if(e.getKeyCode() == KeyEvent.VK_W){
+				
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_A){
+				
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_S){
+	
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_D){
+	
+			}
+				
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			if(Nakshicommon.game == null) {
+				return;
+			}
+			
+			if(e.getKeyCode() == KeyEvent.VK_W){
+				
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_A){
+				
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_S){
+	
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_D){
+	
+			}
+		}
+					
+}
